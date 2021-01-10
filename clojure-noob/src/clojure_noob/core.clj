405b0d928d3291a15c9e13ff485101d158d8a5de
Christian Zachariassen <Christian.Zachariassen@imatis.com>
2021-01-10 09:38:15 +0100

    initial rev.

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,15 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a little man!"))
+
+
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+       (if (= severity :mild)
+         "MILDLY INCONVENIENCED!"
+         "DOOOOOOOMED!")))
